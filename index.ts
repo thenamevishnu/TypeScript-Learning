c03603090c66a054ef3d4c8d5e1d3ebc921bd0f1
thenamevishnu <mail.vishnumk@gmail.com>
2024-02-27 12:32:17 +0530

    Explicit Types

--- index.ts
+++ index.ts
@@ -3,3 +3,16 @@
 let userName = "VishnuMK"
 // userName = 10 => Type 'number' is not assignable to type 'string'.
 
+// Defining Types (Explicit Types)
+
+let usersName: string = "VishnuMK"
+let followers: number = 10_000
+// followers = "VishnuMK" => Type 'string' is not assignable to type 'number'.
+let isFollower: boolean = true
+let followersList: string[] = ["user1", "user2", "user3"]
+// let followingList: string[] = ["user1", 10, "user3"] => Type 'number' is not assignable to type 'string'.
+let userInfo: { name: string, age: number, isActive: boolean, isPremium?: boolean } = {
+    name: "Vishnu",
+    age: 20,
+    isActive: true
+} // isPremium is optional
